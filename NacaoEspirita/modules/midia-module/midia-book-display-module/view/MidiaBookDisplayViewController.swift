d5c4ae8fed74b0e6b460e4fd7b1a7856ffb9ffea
Felippe Matos Francoski <felippe.matos@hotmail.com>
2019-11-14 11:49:28 +0100

    Limpeza antes da implementacao das funcionalidades do reader

--- NacaoEspirita/modules/midia-module/midia-book-display-module/view/MidiaBookDisplayViewController.swift
+++ NacaoEspirita/modules/midia-module/midia-book-display-module/view/MidiaBookDisplayViewController.swift
@@ -24,7 +24,6 @@ class MidiaBookDisplayViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        view.backgroundColor = .white
         setupPDFView()
         setupThumbnailView()
     }
@@ -47,7 +46,6 @@ class MidiaBookDisplayViewController: UIViewController {
     private func setupPDFView() {
         view.addSubview(pdfView)
         pdfView.displayDirection = .horizontal
-//        pdfView.usePageViewController(true)
         pdfView.pageBreakMargins = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         pdfView.displayMode = .singlePageContinuous
         pdfView.autoScales = true
@@ -59,7 +57,6 @@ class MidiaBookDisplayViewController: UIViewController {
         pdfView.addGestureRecognizer(touch)
         pdfView.addGestureRecognizer(doubleTapGesture)
         
-        // HGPDFKit
         pdfView.scrollView?.contentInsetAdjustmentBehavior = .scrollableAxes
         pdfView.getScaleFactorForSizeToFit()
         pdfView.setMinScaleFactorForSizeToFit()
@@ -89,23 +86,6 @@ class MidiaBookDisplayViewController: UIViewController {
         pdfView.autoZoomInOrOut(location: sender.location(in: pdfView), animated: true)
     }
     
-//    private func setupDismissButton() {
-//        dismissButton = UIButton(frame: CGRect(x: 30, y: 45, width: 60, height: 60))
-//        dismissButton.layer.cornerRadius = dismissButton.frame.width/2
-//        dismissButton.setTitle("X", for: .normal)
-//        dismissButton.titleLabel?.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 30)
-//        dismissButton.titleEdgeInsets = UIEdgeInsets(top: 5, left: 0, bottom: 0, right: 0)
-//        dismissButton.setTitleColor(.white, for: .normal)
-//        dismissButton.backgroundColor = .black
-//        dismissButton.alpha = 0.8
-//        view.addSubview(dismissButton)
-//        dismissButton.addTarget(self, action: #selector(back), for: .touchUpInside)
-//    }
-    
-//    @objc private func back() {
-//        self.dismiss(animated: true, completion: nil)
-//    }
-    
     private func setupThumbnailView() {
         thumbnailView.pdfView = pdfView
         thumbnailView.backgroundColor = UIColor(displayP3Red: 179/255, green: 179/255, blue: 179/255, alpha: 0.5)
@@ -115,51 +95,7 @@ class MidiaBookDisplayViewController: UIViewController {
         view.addSubview(thumbnailView)
     }
     
-//    private func setupOutlineButton() {
-//        outlineButton = UIButton(frame: CGRect(x: view.frame.maxX - 90, y: 45, width: 60, height: 60))
-//        outlineButton.layer.cornerRadius = outlineButton.frame.width/2
-//        outlineButton.setTitle("三", for: .normal)
-//        outlineButton.titleLabel?.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 30)
-//        outlineButton.titleEdgeInsets = UIEdgeInsets(top: 5, left: 0, bottom: 0, right: 0)
-//        outlineButton.setTitleColor(.white, for: .normal)
-//        outlineButton.backgroundColor = .black
-//        outlineButton.alpha = 0.8
-//        view.addSubview(outlineButton)
-//        outlineButton.addTarget(self, action: #selector(toggleOutline(sender:)), for: .touchUpInside)
-//    }
-//
-//    @objc private func toggleOutline(sender: UIButton) {
-//
-//        guard let outline = self.outline else {
-//            print("PDF has no outline")
-//            return
-//        }
-//
-//        let outlineViewController = OutlineTableViewController(outline: outline, delegate: self)
-//        outlineViewController.preferredContentSize = CGSize(width: 300, height: 400)
-//        outlineViewController.modalPresentationStyle = UIModalPresentationStyle.popover
-//
-//        let popoverPresentationController = outlineViewController.popoverPresentationController
-//        popoverPresentationController?.sourceView = outlineButton
-//        popoverPresentationController?.sourceRect = CGRect(x: sender.frame.width/2, y: sender.frame.height, width: 0, height: 0)
-//        popoverPresentationController?.permittedArrowDirections = UIPopoverArrowDirection.up
-//        popoverPresentationController?.delegate = self
-//
-//        self.present(outlineViewController, animated: true, completion: nil)
-//    }
 }
-
-//extension MidiaBookDisplayViewController: OutlineDelegate {
-//    func goTo(page: PDFPage) {
-//        pdfView.go(to: page)
-//    }
-//}
-
-//extension MidiaBookDisplayViewController: UIPopoverPresentationControllerDelegate {
-//    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
-//        return .none
-//    }
-//}
 
 extension UIView {
     func addConstraintsWithFormat(format: String, views: UIView...) {
